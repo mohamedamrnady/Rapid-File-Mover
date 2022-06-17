2d89c98e9039d8b97d883c86b0bc2b5843bf1e4d
Mohamed Amr Nady <mohamed.amr.nes@gmail.com>
2022-06-17 17:13:44 +0200

    Added 'scan folders loop' function to get all subfolders and some helping comments

--- Rapid_File_Mover.py
+++ Rapid_File_Mover.py
@@ -9,7 +9,10 @@ root = Tk()
 root.title = "Rapid File Mover"
 root.withdraw()
 
-def start_process(target):
+# function
+
+#mover
+def start_moving(target):
     # target data
     target_stats = os.stat(target)
     target_filename = os.path.basename(target)
@@ -52,25 +55,40 @@ def start_process(target):
     print("System might take some time to identify the moving process")
     print("Please Be Patient!")   
 
+# scan subfolders loop
+def fast_scandir(dirname):
+    subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
+    for dirname in list(subfolders):
+        subfolders.extend(fast_scandir(dirname))
+    return subfolders
+
+#program starts here
+
 # dialog to get user choice of file
 intial_folder = filedialog.askdirectory(title="Select Folder")
 
 
 # checks wether user selected target or cancelled
 if intial_folder:
-    folder_list = []
+    subfolders_list = fast_scandir(intial_folder)
     # dialog to get user choice of destination
     destination = filedialog.askdirectory(title="Select Destination")
     if destination:
         destination = os.path.join(destination, os.path.basename(intial_folder))
         if os.path.exists(destination) == False:
             os.mkdir(destination)
-        for subfolder_name in os.listdir(intial_folder):
-            target = intial_folder + '/' + subfolder_name
-            if os.path.isfile(target):
-                start_process(target)
-            if os.path.isdir(target):
-                folder_list.append(target)
-    print(folder_list)     
+        for subfolder_name in subfolders_list:
+            destination = subfolder_name.replace(intial_folder,destination)
+            print(destination)
+            input('PAUSED')
+            #destination_subfolder_name = os.path.join(destination, os.path.basename(subfolder_name))
+            if os.path.exists(destination) == False:
+               os.mkdir(destination)
+            #if os.path.exists(destination_subfolder_name) == False:
+            #    os.mkdir(destination_subfolder_name)
+            for subitem_name in os.listdir(subfolder_name):
+                target = subfolder_name + '/' + subitem_name
+                if os.path.isfile(target):
+                    start_moving(target)
     # confirming user input to prevent weird silent ending 
     input("Press ENTER to continue.")       
